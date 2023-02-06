98357b3fa313a3a144c6fa0a8322a52f9fb9ee59
emorton23 <122847881+emorton23@users.noreply.github.com>
2023-02-06 15:49:47 -0600

    Upload Homework 3

---
+++ Homework3.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework3;
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Console.WriteLine("Assign an int value to N:");
+        int i = Convert.ToInt16(Console.ReadLine());
+                for(int j=1;j<=i;j++){
+                    for(int k=1;k<=i;k++){
+                        if(j>=k)
+                            Console.Write(j);
+
+                        
+                    }
+                Console.WriteLine("");
+                 }
+
+
+    }
+
+}
+
+
+
